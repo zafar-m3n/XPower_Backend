eca632e54f814afa004f195aa9a885c4476081e0
zafar-m3n <thezafar.m3n@gmail.com>
2025-07-29 17:05:13 +0530

    Product Model

---
+++ models/Product.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
+
+const Product = sequelize.define(
+  "Product",
+  {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    name: { type: DataTypes.STRING(150), allowNull: false },
+    code: { type: DataTypes.STRING(100), allowNull: false, unique: true },
+    description: { type: DataTypes.TEXT, allowNull: true },
+    brand: { type: DataTypes.STRING(100), allowNull: true },
+    cost: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
+    category_id: { type: DataTypes.INTEGER, allowNull: true },
+  },
+  {
+    tableName: "products",
+    timestamps: true,
+    underscored: true,
+  }
+);
+
+module.exports = Product;
