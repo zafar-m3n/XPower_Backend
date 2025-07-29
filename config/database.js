77d273dc5d9d692111def65d94a29a5bbbdf4401
zafar-m3n <thezafar.m3n@gmail.com>
2025-07-29 16:58:48 +0530

    Database.js

---
+++ config/database.js
@@ -0,0 +1,28 @@
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+const colors = require("colors");
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.NODE_XPOWER_MYSQL_DATABASE,
+  process.env.NODE_XPOWER_MYSQL_USER,
+  process.env.NODE_XPOWER_MYSQL_PASSWORD,
+  {
+    host: process.env.NODE_XPOWER_MYSQL_HOST,
+    dialect: "mysql",
+    logging: false,
+  }
+);
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(`MySQL Connected: ${sequelize.config.host}`.bgGreen.white);
+  } catch (err) {
+    console.error(`Error: ${err.message}`.bgRed.white);
+    process.exit(1);
+  }
+};
+
+module.exports = { sequelize, connectDB };
